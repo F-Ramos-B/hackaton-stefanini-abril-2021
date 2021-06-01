6c8440742f62feb1e8fb5e86435d53c71e42e25d
Francisco Ramos Bisneto <framos.df@gmail.com>
2021-05-31 21:05:25 -0300

    Correção tipo em user-cursos

--- stefacoin/src/models/user-cursos.model.ts
+++ stefacoin/src/models/user-cursos.model.ts
@@ -1,14 +1,15 @@
 import Curso from '../entities/curso.entity';
 import Usuario from '../entities/usuario.entity';
 import Mapper from '../utils/mapper';
+import CursoProfessor from './curso-professor.model';
 
 export default class UsuarioCursos<T extends Usuario> {
   id: number
   nome: string;
   email: string;
-  cursos: Curso[];
+  cursos: CursoProfessor[] | Curso[];
 
-  constructor(usuario: T, cursos: Curso[]) {
+  constructor(usuario: T, cursos: CursoProfessor[] | Curso[]) {
     return {
       ...Mapper.copy(usuario, ['senha', 'tipo', 'cursos'] as Array<keyof T>),
       cursos
